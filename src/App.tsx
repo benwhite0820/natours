3e711122a9c1c20d046774c24c9396a704eb0416
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-28 15:58:57 +0800

    building the header part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import LogoWhite from './assets/images/logo-white.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <div className="logo-box">
+          <img src={LogoWhite} alt="logo" className="logo" />
+        </div>
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">Outdoors</span>
+            <span className="heading-primary-sub">is where life happens</span>
+          </h1>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
