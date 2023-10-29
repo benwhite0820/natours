d9dea57de5da033eba2907af941e71bb02a8c909
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-29 14:06:26 +0800

    implementing BEM in the Natours project

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ function App() {
   return (
     <>
       <header className="header">
-        <div className="logo-box">
-          <img src={LogoWhite} alt="logo" className="logo" />
+        <div className="header__logo-box">
+          <img src={LogoWhite} alt="logo" className="header__logo" />
         </div>
-        <div className="text-box">
+        <div className="header__text-box">
           <h1 className="heading-primary">
-            <span className="heading-primary-main">Outdoors</span>
-            <span className="heading-primary-sub">is where life happens</span>
+            <span className="heading-primary--main">Outdoors</span>
+            <span className="heading-primary--sub">is where life happens</span>
           </h1>
 
-          <a href="#" className="btn btn-white btn-animated">
+          <a href="#" className="btn btn--white btn--animated">
             Discover our tours
           </a>
         </div>
