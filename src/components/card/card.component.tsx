753d71eb714caa5e154bab5419850e503271f1c4
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-30 21:54:51 +0800

    building the tours section - part 2

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,21 +1,43 @@
 import './card.style.scss';
 
 type Props = {
-  classNameForFront?: string;
-  classNameForBack?: string;
+  backClassName?: string;
+  imageClassName?: string;
+  cardTitleClassName?: string;
+  cardTitle?: string;
+  detailsArray?: string[];
 };
 
-const Card = ({ classNameForFront = '', classNameForBack = '' }: Props) => {
+const Card = ({
+  backClassName = '',
+  imageClassName = '',
+  cardTitleClassName = '',
+  cardTitle,
+  detailsArray,
+}: Props) => {
   return (
     <div className="card">
-      <div className={`card__side card__side--front ${classNameForFront}`}>
-        Front
+      <div className="card__side card__side--front">
+        {/* card__picture 給blend-mode */}
+        {/* imageClassName 給沒有透明度的背景圖片加 linear-gradient */}
+        <div className={`card__picture ${imageClassName}`} />
+
+        <h4 className="card__heading">
+          <span className={`card__heading-span ${cardTitleClassName}`}>
+            {cardTitle}
+          </span>
+        </h4>
+
+        <div className="card__details">
+          <ul>
+            {detailsArray?.map((detail) => (
+              <li key={detail}>{detail}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div
-        className={`card__side card__side--back card__side--back-1 ${classNameForBack}`}
-      >
-        Back
-      </div>
+
+      <div className={`card__side card__side--back ${backClassName}`}>Back</div>
     </div>
   );
 };
--- src/pages/section-tours/section-tours.page.tsx
+++ src/pages/section-tours/section-tours.page.tsx
@@ -5,6 +5,16 @@ import Grid from '@components/grid/grid.component';
 import Heading from '@components/heading/heading.component';
 
 const SectionTours = () => {
+  const dataObject = {
+    card1: [
+      '3 day tours',
+      'Up to 30 people',
+      '2 tour guides',
+      'Sleep in cozy hotels',
+      'Difficulty: ease',
+    ],
+  };
+
   return (
     <section className="section-tours">
       <div className="u-center-text u-margin-bottom-big">
@@ -12,7 +22,25 @@ const SectionTours = () => {
       </div>
       <Grid row>
         <Grid colClassName="col-1-of-3">
-          <Card classNameForBack="card__side--front-1" />
+          <Card
+            backClassName="card__side--back-1"
+            imageClassName="card__picture--1"
+            cardTitleClassName="card__heading-span--1"
+            cardTitle="The Sea Explorer"
+            detailsArray={dataObject.card1}
+          />
+        </Grid>
+        <Grid colClassName="col-1-of-3">
+          <Card
+            backClassName="card__side--back-2"
+            cardTitle="The Sea Explorer"
+          />
+        </Grid>
+        <Grid colClassName="col-1-of-3">
+          <Card
+            backClassName="card__side--back-3"
+            cardTitle="The Sea Explorer"
+          />
         </Grid>
       </Grid>
     </section>
